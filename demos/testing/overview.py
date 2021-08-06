6e04d1654543e67af8c13609b629589ca2f52d76
jan.reising@ki.se <jan.reising@ki.se>
2021-08-06 14:15:02 +0200

    testing regarding switch from mc to cnmfe

---
+++ demos/testing/overview.py
@@ -0,0 +1,32 @@
+import getopt
+import h5py as h5
+import os, sys
+
+def main(path):
+
+    with h5.File(path, "a") as file:
+        for key in list(file.keys()):
+
+            print(key, type(file[key]))
+
+
+
+
+if __name__ == "__main__":
+
+    input_file = None
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "i:", ["ifolder="])
+    except getopt.GetoptError:
+        print("calpack.py -i <input_file>")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-i", "--input_file"):
+            input_file = arg
+
+    assert os.path.isfile(input_file), "input_file is not a file: {}".format(input_file)
+
+    print("InputFile: ", input_file)
+
+    main(input_file)
