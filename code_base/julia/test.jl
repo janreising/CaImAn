7d4d06aa4d3034b793ce2c4c999e0126258d91e9
jan.reising@ki.se <jan.reising@ki.se>
2021-09-16 11:53:35 +0200

    bin_median test function added

--- code_base/julia/test.jl
+++ code_base/julia/test.jl
@@ -1,22 +1,44 @@
-# Load libraries
+# Load Julia libraries
 using PyCall
+using Conda
 using Test
+
+# Load Python libraries
+#Conda.add("opencv")
 
 # include python directory to look for modules
 pushfirst!(PyVector(pyimport("sys")."path"), "../to_julia/")
-pushfirst!(PyVector(pyimport("sys")."path"), "")
 
 # make imports
-mf = pyimport("myfunc")
 cp_mc = pyimport("cp_motioncorrection")
 
-# Do something
-function jl_call_me(num)
-	return num+num
+# Julia functions -> load from package in future
+function bin_median(A, window)
+	# TODO implement
+	return false
 end
 
-n = 3
-@test jl_call_me(n) == mf.standalone_call_me(n)
+function extract_shifts(A, max_shift_w, max_shift_h, template, method)
+	# TODO implement
+	return false
+end
+
+# Testing
+@testset "MotionCorrection" begin
+	# TODO generate something that is closer to 
+	# reality or maybe just actual data
+	A = rand(Int, (40, 100, 100))
+	
+	window = 10
+	#@test cp_mc.movie.bin_median(A, window) == bin_median(A, window)
+
+	template = A[20, :, :]
+	max_shift_h = 50
+        max_shift_w = 50
+        method = "opencv"
+	#@test cp_mc.movie.extract_shifts(A, max_shift_w, max_shift_h, template=template, method=method) == extract_shifts(A, max_shift_w, max_shift_h, template=template, method=method)
+
+end
 
 # Close
-print("\nDone\n")
+print("\nAll tests finished successfully! Good job\n")
