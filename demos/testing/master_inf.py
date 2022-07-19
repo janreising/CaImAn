ac789607feb3741e248122d26f63a65fdfcca1f4
Jan Philipp Reising <jan.reising@ki.se>
2022-07-19 11:43:41 +0200

    adjusted pipeline to fit inference

--- demos/testing/master_inf.py
+++ demos/testing/master_inf.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
 
         if len(missing_inf) > 0:
             for loc in missing_inf:
-                INF = Inference(input_, "~/Private/starsmooth/res/model.h5", loc=loc)
+                INF = Inference(input_, "home/j/janrei/Private/starsmooth/res/model.h5", loc=loc)
                 INF.run()
         else:
             print("*MASTER* INF found!")
